7dfafe5b58a061256200d90c196821c5f3173108
Jarvo <34064987+JarvoDiscord@users.noreply.github.com>
2022-09-03 03:39:02 +0200

    Create interactionCreate.js

---
+++ Events/Interaction/interactionCreate.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+const Config = require("../../config.json")
+
+module.exports = async (client, interaction) => {
+    if (!interaction.isCommand() && !interaction.isContextMenu() && !interaction.isButton()) return; // When the interaction is not a command, not a contextmenu, or not a button, it will not execute.
+
+    if (interaction.isCommand() || interaction.isContextMenu()) { // If the command is a command or an contextmenu, it will run the below code.
+        const command = client.commands.get(interaction.commandName) // This is the command (It's the same for ContextMenu as a ContextMenuCommand is just the same as a slash command, only the difference is that ContextMenuCommands are ran through an User Interface.
+        if (!command) return // If the command does not exists, return again.
+
+        try {
+            await command.execute(interaction) // Try to execute the command.
+        } catch (err) {
+            if (err) console.error(err) // If it fails, it returns an error.
+
+            await interaction.reply({ // And you inform the users that you have found an error.
+                content: `You found an error!`,
+                ephemeral: true
+            })
+        }
+    }
+}
