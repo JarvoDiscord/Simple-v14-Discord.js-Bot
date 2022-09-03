1bf9b5d68f18f089669c8c71cbfea9b6dbe17521
Jarvo <34064987+JarvoDiscord@users.noreply.github.com>
2022-09-03 03:35:37 +0200

    Create hi.js

---
+++ Commands/hi.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+const {
+    SlashCommandBuilder
+} = require("@discordjs/builders")
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("hi") // The name of the slash command
+    .setDescription("Say's Hi!"), // A short description about the slash command.
+  
+  async execute(interaction) {
+    
+    interaction.reply({ // You always should reply to an interaction. If you want the interaction to reply to send another message, use interaction.followUp()
+      content: "Hello there!",
+      ephemeral: true // You set this to true if you want the message only be visible by the one who ran the command. Nobody else will see it and the message will disappear after a while.
+    })
+  }
+}
