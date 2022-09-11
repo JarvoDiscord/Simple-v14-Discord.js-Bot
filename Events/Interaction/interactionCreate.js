64ce73770ae7e4b14902063335e883618062d951
Jarvo <34064987+JarvoDiscord@users.noreply.github.com>
2022-09-12 00:31:45 +0200

    Removed some lines
    As we don't have multiple interactions that should execute, it will work fine like this as well.

--- Events/Interaction/interactionCreate.js
+++ Events/Interaction/interactionCreate.js
@@ -2,21 +2,19 @@ const Discord = require("discord.js")
 const Config = require("../../config.json")
 
 module.exports = async (client, interaction) => {
-    if (!interaction.isCommand() && !interaction.isContextMenuCommand() && !interaction.isButton()) return; // When the interaction is not a command, not a contextmenu, or not a button, it will not execute.
-
-    if (interaction.isCommand() || interaction.isContextMenuCommand()) { // If the command is a command or an contextmenu, it will run the below code.
-        const command = client.commands.get(interaction.commandName) // This is the command (It's the same for ContextMenu as a ContextMenuCommand is just the same as a slash command, only the difference is that ContextMenuCommands are ran through an User Interface.
-        if (!command) return // If the command does not exists, return again.
-
-        try {
-            await command.execute(interaction) // Try to execute the command.
-        } catch (err) {
-            if (err) console.error(err) // If it fails, it returns an error.
-
-            await interaction.reply({ // And you inform the users that you have found an error.
-                content: `You found an error!`,
-                ephemeral: true
-            })
-        }
+    if (!interaction.isCommand() && !interaction.isContextMenuCommand()) return; // When the interaction is not a command, not a contextmenu, or not a button, it will not execute.
+    
+    const command = client.commands.get(interaction.commandName) // This is the command (It's the same for ContextMenu as a ContextMenuCommand is just the same as a slash command, only the difference is that ContextMenuCommands are ran through an User Interface.
+    if (!command) return // If the command does not exists, return again.
+    
+    try {
+        await command.execute(interaction) // Try to execute the command.
+    } catch (err) {
+        if (err) console.error(err) // If it fails, it returns an error.
+        
+        await interaction.reply({ // And you inform the users that you have found an error.
+            content: `You found an error!`,
+            ephemeral: true
+        })
     }
 }
